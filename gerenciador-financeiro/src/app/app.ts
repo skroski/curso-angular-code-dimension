bd7ebf8215c442159d488021c72eeba064ef40d9
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2025-09-17 07:45:40 -0300

    feat: instalação do material design

--- gerenciador-financeiro/src/app/app.ts
+++ gerenciador-financeiro/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Layout } from './core/layout/layout';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Layout],
+  imports: [Layout, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
