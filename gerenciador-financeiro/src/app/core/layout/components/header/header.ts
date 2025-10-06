f36e033f3579a51451801345b50c6b0437600e45
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2025-10-05 21:24:38 -0300

    feat: uso do material design com o toolbar

--- gerenciador-financeiro/src/app/core/layout/components/header/header.ts
+++ gerenciador-financeiro/src/app/core/layout/components/header/header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
--- gerenciador-financeiro/src/app/core/layout/layout.ts
+++ gerenciador-financeiro/src/app/core/layout/layout.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Header } from './components/header/header';
+// Import the Material button module
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header],
+  imports: [Header, MatButtonModule],
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrl: './layout.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Layout {
 
