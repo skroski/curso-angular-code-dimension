a5cd485756c91c08eed971164325a705506c6255
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2025-10-11 19:37:08 -0300

    feat: criação de rotas

--- gerenciador-financeiro/src/app/app.routes.ts
+++ gerenciador-financeiro/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
